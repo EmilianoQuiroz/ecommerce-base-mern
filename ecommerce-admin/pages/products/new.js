1ce4eafe3d72521ea0b3f1f9e6c2dce138428fa7
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-07-06 23:59:25 -0300

    Formulario para agregar producto

--- ecommerce-admin/pages/products/new.js
+++ ecommerce-admin/pages/products/new.js
@@ -1,32 +1,52 @@
-import Layout from "@/components/Layout"
-import { useState } from "react"
-
+import Layout from "@/components/Layout";
+import { useState } from "react";
+import axios from "axios";
 
 export default function NewProduct() {
 
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [price, setPrice] = useState('');
+    async function createProduct(ev){
+        ev.preventDefault();
+        const data = {title,description,price};
+        await axios.post('/api/products', data)
+    }
   
     return (
     <Layout>
-        <h1>Agregar producto</h1>
+        <form onSubmit={createProduct}>
+            <h1>Agregar producto</h1>
 
-        <label>Nombre del producto</label>
-        <input 
-            type="text" 
-            placeholder="product name" 
-            value={title} 
-            onChange={ev => setTitle(ev.target.value)}
-        />
+            <label>Nombre del producto</label>
+            <input 
+                type="text" 
+                placeholder="product name" 
+                value={title} 
+                onChange={ev => setTitle(ev.target.value)}
+            />
 
-        <label>Descripcion del producto</label>
-        <textarea placeholder="description"></textarea>
+            <label>Descripcion del producto</label>
+            <textarea 
+                placeholder="description" 
+                value={description}    
+                onChange={ev => setDescription(ev.target.value)}
+            />
 
-        <label>Precio</label>
-        <input type="number" placeholder="price" />
+            <label>Precio</label>
+            <input 
+                type="number" 
+                placeholder="price" 
+                value={price}
+                onChange={ev => setPrice(ev.target.value)}
+                />
 
-        <button className="btn-primary">Guardar</button>
+            <button 
+                type="submit" 
+                className="btn-primary">
+                Guardar
+            </button>
+        </form>
     </Layout>
   )
 }
